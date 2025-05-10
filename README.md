8a33efcf7ce368b7eb1032986d52cc4ba1de4d6d
Nguyen Gia Huy <dev@z4hy.me>
2025-05-10 08:35:31 +0000

    Initial works

---
+++ README.md
@@ -0,0 +1,5 @@
+# Ứng dụng quản lý nhà hàng
+
+Thực hiện bởi nhóm 02:
+  * (`2001221670`) Mai Quốc Huy
+  * (`2001221654`) Nguyễn Gia Huy
---
+++ android/app/src/main/kotlin/edu/huit/ltdd/nhom2/nhom2_quanlynhahang/MainActivity.kt
@@ -0,0 +1,5 @@
+package edu.huit.ltdd.nhom2.nhom2_quanlynhahang
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
